7ae4d43238c14dbb7981f9e1d2f24dc0f277f5ed
Martin Hochel <martinhochel@microsoft.com>
2023-01-17 19:14:43 +0100

    chore(scripts-puppeteer): disable --single-process on CI

--- scripts/puppeteer/src/config.ts
+++ scripts/puppeteer/src/config.ts
@@ -3,7 +3,7 @@ import { LaunchOptions } from './types';
 /** Common set of args to be passed to Chromium. */
 const chromiumArgs = [
   // Workaround for newPage hang in CircleCI: https://github.com/GoogleChrome/puppeteer/issues/1409#issuecomment-453845568
-  process.env.TF_BUILD && '--single-process',
+  // process.env.TF_BUILD && '--single-process',
 ].filter(Boolean) as NonNullable<LaunchOptions['args']>;
 
 export const safeLaunchOptions = (launchOptions?: LaunchOptions): LaunchOptions => {
